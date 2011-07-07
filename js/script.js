346c768b5df184d58da2c9c6a6ce95c347379854
Cheston Lee <chestonlloyd@gmail.com>
2011-07-06 17:47:38 -0700

    damn it I hate you git

--- js/script.js
+++ js/script.js
@@ -2,13 +2,38 @@
  *
  */
 $(document).ready(function() {
-    $('#signup').css('display', 'none');
-    $('#where').css('display', 'none');
+    //Make the jsonp request to twitter to get the latest tweets with the
+    //tag hackfortress.
+    var url = 'http://api.twitter.com/1/statuses/user_timeline.json?callback=?';
+    $.getJSON(url, {
+        screen_name : 'hackfortress',
+        count : 5,
+    }, function (resp) {
+        if (resp.length > 0) {
+            var tweetCont = $('#tweetCont');
+            for (var i = 0; i < resp.length; i++) {
+                var div = $('<div>');
+                var text = $('<div>').html($.trim(resp[i].text)).addClass('tweetText');
+                div.append(text);
+                div.attr('class', 'tweet');
+                tweetCont.append(div);
+            }
+            tweetCont.css('display', 'block');
+        } 
+    });
+
+    var currPage = window.location.hash || '#what';
+
+    if (currPage) {
+        $('li > a[href="' + currPage + '"]').addClass('selectedLink');
+        $(currPage).css('display', 'block').addClass('selected');
+    }
+
     //Declare data relationships between nav links and content
     $('li > a').click(function() {
         $('.selectedLink').removeClass('selectedLink');
         $(this).addClass('selectedLink');
-        var str = $($(this).attr('rel'));
+        var str = $($(this).attr('href'));
 
         if (!str.hasClass('selected')) {
             var selected = $('.selected');
@@ -22,25 +47,5 @@ $(document).ready(function() {
             });
         }
 
-    });
-    //Make the jsonp request to twitter to get the latest tweets with the
-    //tag hackfortress.
-    var url = 'http://search.twitter.com/search.json?callback=?';
-    $.getJSON(url, {
-        q : '#hackfortress',
-        rpp : 5,
-        page : 1,
-    }, function (resp) {
-        console.log(resp);
-            for (item in resp.results) {
-                var tweet = resp.results[item];
-                var div = $('<div>');
-                var text = $('<div>').html($.trim(tweet.text)).addClass('tweetText');
-                var user = $('<div>').html($.trim(tweet.from_user)).addClass('tweetUser');
-                div.append(text);
-                div.append(user);
-                div.attr('class', 'tweet');
-                $('#tweetCont').append(div);
-            }
     });
 });
